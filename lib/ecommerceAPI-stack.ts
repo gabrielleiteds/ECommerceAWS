0bbeccf1e95c2bf9567ac3b99fa8608356989fbd
gabrielleiteds <cloudhousegls@gmail.com>
2024-05-28 14:50:46 -0300

    chore: adjust stacks to run deploy

--- lib/ecommerceAPI-stack.ts
+++ lib/ecommerceAPI-stack.ts
@@ -4,13 +4,39 @@ import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as cwlogs from 'aws-cdk-lib/aws-logs';
 
+interface ECommerceAPIStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJS.NodejsFunction;
+}
+
 // class to create API Gateway Stack
 export class ECommerceAPIStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ECommerceAPIStackProps) {
     super(scope, id, props);
 
+    const logGroup = new cwlogs.LogGroup(this, 'ECommerceAPILogs');
     const api = new apigateway.RestApi(this, 'ECommerceAPI', {
       restApiName: 'ECommerceAPI',
+      cloudWatchRole: true,
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          caller: true,
+          responseLength: true,
+          status: true,
+          user: true,
+        }),
+      },
     });
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFetchHandler,
+    );
+    const productsResource = api.root.addResource('products');
+    productsResource.addMethod('GET', productsFetchIntegration);
   }
 }
